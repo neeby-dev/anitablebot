77acd57fdc820319a7a958b1c0fede4217822081
3xepy <3xepy.dev@gmail.com>
2021-09-23 20:27:12 +0300

    Removed Boto

--- config/config.py
+++ config/config.py
@@ -1,9 +1,8 @@
 import os
-from boto.s3.connection import S3Connection
 
 # Heroku
-TELEGRAM_TOKEN = S3Connection(os.environ['TELEGRAM_TOKEN'])
-DATABASE_URL = S3Connection(os.environ['DATABASE_URL'])
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # Local
 # TELEGRAM_TOKEN = ''
